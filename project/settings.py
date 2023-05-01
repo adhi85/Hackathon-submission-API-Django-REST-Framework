17b84807e44b496257ef103daee3ef87a68736c5
adhi85 <adimuthanikatt@gmail.com>
2023-05-02 02:14:35 +0530

    deploying

--- project/settings.py
+++ project/settings.py
@@ -85,23 +85,27 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hackathon',
+        'ENGINE':'mysql://root:lvmPvaG5SeG5aEeZ2Ib6@containers-us-west-37.railway.app:6902/railway',
+        'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'muthanikatt',
-        'PORT': 3306,
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'lvmPvaG5SeG5aEeZ2Ib6',
+        'PORT': 6902,
+        'HOST': 'containers-us-west-37.railway.app',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'hackathon',
+#         'USER': 'root',
+#         'PASSWORD': 'muthanikatt',
+#         'PORT': 3306,
+#         'HOST': '127.0.0.1',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
